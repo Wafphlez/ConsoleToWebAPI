6314c9ba72056f0c9adc1aa1e03e71785ba95293
Alexander <wafphlez@gmail.com>
2022-01-31 22:48:09 +0300

    Initial with basic endpoints

---
+++ ConsoleToWebAPI/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace ConsoleToWebAPI
+{
+    internal class Program
+    {
+        // 1. Reconfigure Project
+        // 2. Create Host Builder
+        // 3. Configure Web Hosts
+        //    Startup
+        //      ConfigureServices
+        //      Configure
+        // 4. Routing
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webHost =>
+                {
+                    webHost.UseStartup<Startup>();
+                });
+        }
+
+    }
+}
---
+++ ConsoleToWebAPI/Startup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConsoleToWebAPI
+{
+    public class Startup
+    {
+        public void ConfigureWebServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello from async response");
+                });
+
+                endpoints.MapGet("/new", async context =>
+                {
+                    await context.Response.WriteAsync("New endpoint reached");
+                });
+
+                endpoints.MapGet("/old", async context =>
+                {
+                    await context.Response.WriteAsync("OLD endpoint reached");
+                });
+            });
+        }
+    }
+}
