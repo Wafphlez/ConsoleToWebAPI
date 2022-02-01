db3a17baa1496c174255f9dbd47837ad0ade81f7
Alexander <wafphlez@gmail.com>
2022-02-01 11:59:28 +0300

    Controllers

---
+++ ConsoleToWebAPI/CustomMiddleware1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleToWebAPI
+{
+    public class CustomMiddleware1 : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            await context.Response.WriteAsync("3\n");
+            await next(context);
+            await context.Response.WriteAsync("4\n");
+        }
+    }
+}
